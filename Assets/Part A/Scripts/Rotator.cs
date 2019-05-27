9eb2f3dc7fddf965339cbf39445b318b666d8f26
Bradley Gallardo <bradleygallardo@gmail.com>
2019-05-26 23:39:01 -0700

    Add files via upload

---
+++ Assets/Part A/Scripts/Rotator.cs
@@ -0,0 +1,91 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rotator : MonoBehaviour {
+ 	Renderer render;
+    Color startColor = new Color(1,0,0,1);
+	Color endColor = new Color(0,0,1,1);
+
+	// Use this for initialization
+	void Start () {
+		render = GetComponent<Renderer>();
+        render.material.shader = Shader.Find("Custom/ParticleShader");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		// this rotates each cube.
+		transform.Rotate (new Vector3 (15, 30, 45) * Time.deltaTime);
+
+
+		// --------------------------------------------------------
+		// ------- animate the cube size based on spectrum data.
+
+		// consolidate spectral data to 8 partitions (1 partition for each rotating cube)
+		int numPartitions = 8;
+		float[] aveMag = new float[numPartitions];
+		float partitionIndx = 0;
+		int numDisplayedBins = 512 / 2;
+
+		for (int i = 0; i < numDisplayedBins; i++)  {
+			if(i < numDisplayedBins * (partitionIndx + 1) / numPartitions) {
+				aveMag[(int)partitionIndx] += AudioPeer.spectrumData [i] / (512/numPartitions);
+			}
+			else{
+				partitionIndx++;
+				--i;
+			}
+		}
+
+		// scale and bound the average magnitude.
+		for(int i = 0; i < numPartitions; i++) {
+			print(i);
+			
+			if (i % 3 == 0)
+				startColor.r = aveMag[i] * (i * 4000);
+			else if (i % 3 == 1)
+				startColor.g = aveMag[i] * (i * 2000);
+			else if (i % 3 == 2)
+				startColor.b = aveMag[i] * (i * 1000);
+			
+			aveMag[i] = (float)0.5 + aveMag[i]*100;
+			
+			
+			if (aveMag[i] > 100) {
+				aveMag[i] = 100;
+				startColor.r = aveMag[i];
+				startColor.g = aveMag[i];
+				startColor.b = aveMag[i];
+			}
+		}
+
+		transform.localScale = new Vector3 (aveMag[0], aveMag[0], aveMag[0]);
+		
+		if (Input.GetKeyDown("left")) {
+			endColor.r -= 0.1f;
+        }
+        if (Input.GetKeyDown("right")) {
+			endColor.r += 0.1f;
+        }
+		if (Input.GetKeyDown("up")) {
+			endColor.g -= 0.1f;
+		}
+		if (Input.GetKeyDown("down")) {
+			endColor.g += 0.1f;
+		}
+
+		//endColor.r = aveMag[0];
+		//endColor.g = aveMag[0];
+		//endColor.b = aveMag[0];
+ 
+		render.material.SetColor("_StartColor", startColor);
+		render.material.SetColor("_EndColor", endColor);
+
+		// --------- End animating cube via spectral data
+		// --------------------------------------------------------
+	}
+
+
+}
+
---
+++ Assets/Part A/Shaders/ParticleShader.shader
@@ -0,0 +1,60 @@
+﻿Shader "Custom/ParticleShader"
+{
+    Properties {
+        _MainTex ("Texture", 2D) = "white" {}
+        //Define properties for Start and End Color
+        _StartColor ("Start Color", Color) = (1,1,1,1)
+        _EndColor ("End Color", Color) = (1,1,1,1)
+
+    }
+    SubShader {
+        Tags {"Queue"="Transparent" "RenderType"="Opaque" }
+        LOD 100
+        
+        Blend One One
+        ZWrite off
+        Pass {
+            CGPROGRAM
+            #pragma vertex vert
+            #pragma fragment frag
+
+            uniform float4 _StartColor;
+            uniform float4 _EndColor;
+
+            struct appdata {
+                float4 vertex : POSITION;
+                float3 uv : TEXCOORD0;
+            };
+
+            struct v2f {   
+                float4 vertex : SV_POSITION;
+                float3 uv : TEXCOORD0;
+            };
+
+           uniform sampler2D _MainTex;
+          
+
+            v2f vert (appdata v) {
+                v2f o;
+                o.vertex = UnityObjectToClipPos(v.vertex);
+                o.uv = v.uv; 
+             
+                return o;
+            }
+
+            float4 frag (v2f i) : SV_Target {
+                //Get particle age percentage
+                float age = i.uv.z;
+
+                //Sample color from particle texture
+                float4 texColor = tex2D(_MainTex, i.uv.xy);
+
+                //Do a linear interpolation of start color and end color based on particle age percentage
+                float4 finalColor = lerp(_StartColor, _EndColor, age) *     (texColor.a);
+
+                return finalColor;
+            }
+            ENDCG
+        }
+    }
+}
